ce9aad8a5409199ba70dfbee95040e66f04934cb
EinsteinYap <45678629+EinsteinYap@users.noreply.github.com>
2021-02-12 16:45:05 +0800

    First Upload for Quasar Vue App

---
+++ src/components/Tasks/TasksCompleted.vue
@@ -0,0 +1,37 @@
+<template>
+  <transition
+    appear
+    enter-active-class="animated zoomIn"
+    leave-active-class="animated zoomOut"
+  >
+    <div class="q-mt-lg">
+      <list-header
+        bgColor="bg-green-4"
+      >Completed</list-header>
+    
+      <q-list
+        separator
+        bordered
+      >
+    
+        <task
+          v-for="(task, key) in tasksCompleted"
+          :key="key"
+          :task="task"
+          :id="key"
+        ></task>
+    
+      </q-list>
+    </div>
+  </transition>
+</template>
+
+<script>
+export default {
+  props: ['tasksCompleted'],
+  components: {
+    'task': require('components/Tasks/Task.vue').default,
+    'list-header': require('components/Shared/ListHeader.vue').default
+  }
+}
+</script>
